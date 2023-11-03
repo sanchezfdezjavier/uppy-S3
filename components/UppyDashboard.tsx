75bf895217d3a90f884f02c7f504d805ee647632
Javier Sanchez <sanchezfdez.javier@gmail.com>
2023-11-02 19:29:28 -0700

    Added TODOs

--- components/UppyDashboard.tsx
+++ components/UppyDashboard.tsx
@@ -1,13 +1,13 @@
 "use client"
+// TODO: Setup allowed files
+// TODO: Type stuff
 import React, { useEffect } from 'react';
 import Uppy from '@uppy/core';
-import Webcam from '@uppy/webcam';
-import { Dashboard, ProgressBar } from '@uppy/react';
+import { Dashboard, ProgressBar, StatusBar } from '@uppy/react';
 
 import '@uppy/core/dist/style.min.css';
 import '@uppy/dashboard/dist/style.min.css';
 import '@uppy/webcam/dist/style.min.css';
-import DragDrop from '@uppy/drag-drop';
 
 const uppy = new Uppy()
 
@@ -16,7 +16,7 @@ export default function UppyDashboard(props) {
         uppy.setOptions({ restrictions: props.restrictions });
     }, [props.restrictions]);
 
-    return <div className='p-2 bg-gradient-to-t from-pink-600 to-orange-500 w-8/12 h-8/12 rounded-xl'>
-        <Dashboard uppy={uppy} plugins={['Webcam']} />
-        </div>;
+    return <div className='p-2 bg-gradient-to-tr from-pink-600 to-orange-500 w-8/12 h-8/12 rounded-xl shadow-2xl'>
+                <Dashboard uppy={uppy} plugins={['Webcam']} />
+            </div>;
 }
