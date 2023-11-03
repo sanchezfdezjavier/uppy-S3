bf9f592ffb26541c85e2469d75d9d528483130b8
Javier Sanchez <sanchezfdez.javier@gmail.com>
2023-11-02 19:33:07 -0700

    Added prettier plugin for tailwind + prettier config

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import UppyDashboard from "@/components/UppyDashboard"
+import UppyDashboard from '@/components/UppyDashboard'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-stone-50 space-y-8">
-      <h1 className="text-6xl tracking-tighter font-bold text-transparent bg-clip-text bg-gradient-to-t from-pink-600 to-orange-500 p-2">Uppy + S3 Demo</h1>
-      <UppyDashboard/>
+    <main className="flex min-h-screen flex-col items-center justify-center space-y-8 bg-stone-50">
+      <h1 className="bg-gradient-to-t from-pink-600 to-orange-500 bg-clip-text p-2 text-6xl font-bold tracking-tighter text-transparent">
+        Uppy + S3 Demo
+      </h1>
+      <UppyDashboard />
     </main>
   )
 }
--- components/UppyDashboard.tsx
+++ components/UppyDashboard.tsx
@@ -1,22 +1,24 @@
-"use client"
+'use client'
 // TODO: Setup allowed files
 // TODO: Type stuff
-import React, { useEffect } from 'react';
-import Uppy from '@uppy/core';
-import { Dashboard, ProgressBar, StatusBar } from '@uppy/react';
+import React, { useEffect } from 'react'
+import Uppy from '@uppy/core'
+import { Dashboard } from '@uppy/react'
 
-import '@uppy/core/dist/style.min.css';
-import '@uppy/dashboard/dist/style.min.css';
-import '@uppy/webcam/dist/style.min.css';
+import '@uppy/core/dist/style.min.css'
+import '@uppy/dashboard/dist/style.min.css'
+import '@uppy/webcam/dist/style.min.css'
 
 const uppy = new Uppy()
 
 export default function UppyDashboard(props) {
-    useEffect(() => {
-        uppy.setOptions({ restrictions: props.restrictions });
-    }, [props.restrictions]);
+  useEffect(() => {
+    uppy.setOptions({ restrictions: props.restrictions })
+  }, [props.restrictions])
 
-    return <div className='p-2 bg-gradient-to-tr from-pink-600 to-orange-500 w-8/12 h-8/12 rounded-xl shadow-2xl'>
-                <Dashboard uppy={uppy} plugins={['Webcam']} />
-            </div>;
+  return (
+    <div className="h-8/12 w-8/12 rounded-xl bg-gradient-to-tr from-pink-600 to-orange-500 p-2 shadow-2xl">
+      <Dashboard uppy={uppy} plugins={['Webcam']} />
+    </div>
+  )
 }
